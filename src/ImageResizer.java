e8eb4def23321fcb5adddcfd2a6d7d032566d094
Gavriel770U <kraserultra@gmail.com>
2024-01-27 22:23:13 +0200

    Move all image resizing logic into ImageResizer and use its instance as Panel property

--- src/ImageResizer.java
+++ src/ImageResizer.java
@@ -1,3 +1,6 @@
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
 
 // Singleton for resizing BufferedImage Objects
 
@@ -18,5 +21,31 @@ public class ImageResizer
         }
 
         return instance;
+    }
+
+    public BufferedImage resizeImage(BufferedImage inputImage, final int newWidth, final int newHeight)
+    {
+        BufferedImage resizedImage = new BufferedImage (
+            newWidth,
+            newHeight,
+            inputImage.getType()
+        );
+
+        Graphics2D graphics2D = resizedImage.createGraphics();
+
+        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+        graphics2D.drawImage (
+            inputImage, 
+            0, 
+            0, 
+            newWidth, 
+            newHeight,
+            null
+        );
+
+        graphics2D.dispose();
+
+        return resizedImage;
     }
 }
--- src/Panel.java
+++ src/Panel.java
@@ -3,14 +3,13 @@ import javax.swing.JPanel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Panel extends JPanel
 {
+    private ImageResizer imageResizer;
     private BufferedImage backgroundImage;
 
     public Panel(final String backgroundPath) throws IOException
@@ -20,26 +19,15 @@ public class Panel extends JPanel
         setBackground(Color.BLACK);
         setDoubleBuffered(true);
 
-        this.backgroundImage = ImageIO.read(new File(backgroundPath));
-        
-        BufferedImage resizedImage = new BufferedImage (
-            Settings.FRAME_WIDTH.value,
-            Settings.FRAME_HEIGHT.value,
-            backgroundImage.getType()
-        );
+        this.imageResizer = ImageResizer.getInstance();
 
-        Graphics2D graphics2D = resizedImage.createGraphics();
-        graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        graphics2D.drawImage (
-            this.backgroundImage, 
-            0, 
-            0, 
-            Settings.FRAME_WIDTH.value, 
-            Settings.FRAME_HEIGHT.value,
-            null
+        this.backgroundImage = ImageIO.read(new File(backgroundPath));
+
+        this.backgroundImage = this.imageResizer.resizeImage (
+            this.backgroundImage,
+            Settings.FRAME_WIDTH.value,
+            Settings.FRAME_HEIGHT.value
         );
-        graphics2D.dispose();
-        this.backgroundImage = resizedImage;
     }    
 
     @Override
