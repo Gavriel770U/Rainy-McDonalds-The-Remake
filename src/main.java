bb117f9f9e91dae57a128d885ad66ebab81c47a1
Gavriel Linoy <kraserultra@gmail.com>
2024-02-04 14:44:19 +0200

    Sort all images by sub-folders

--- src/main.java
+++ src/main.java
@@ -7,23 +7,23 @@ class Main
         System.out.println("Welcome!");
 
         ArrayList<FallingObject> fallingObjects = new ArrayList<>();
-        Player player = new Player(Settings.FRAME_WIDTH.value / 2, 600, 160, 160, 10, 0, "./resources/player.png");
+        Player player = new Player(Settings.FRAME_WIDTH.value / 2, 600, 160, 160, 10, 0, "./resources/sprites/player.png");
 
         fallingObjects.add(new FallingObject (
-            100, -100, 100, 100, 5, 10, "./resources/burger.png", "./resources/burgerchew.wav", 10, 8
+            100, -100, 100, 100, 5, 10, "./resources/sprites/burger.png", "./resources/sounds/burgerchew.wav", 10, 8
         ));
 
         fallingObjects.add(new FallingObject (
-            300, -200, 80, 100, 5, 10, "./resources/drink.png", null, 8, 6
+            300, -200, 80, 100, 5, 10, "./resources/sprites/drink.png", null, 8, 6
         ));
 
         fallingObjects.add(new FallingObject (
-            500, -400, 100, 100, 5, 10, "./resources/blackburger.png", "./resources/burgerchew.wav", -10, -8
+            500, -400, 100, 100, 5, 10, "./resources/sprites/blackburger.png", "./resources/sounds/burgerchew.wav", -10, -8
         ));
 
 
         new Frame (
-            "./resources/background.png",
+            "./resources/backgrounds/background.png",
             player,
             fallingObjects
         );
