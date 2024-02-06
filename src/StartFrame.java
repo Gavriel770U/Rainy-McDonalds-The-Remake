cd44ea39b253f76e04762f300a46330cfc3740c7
Gavriel770U <kraserultra@gmail.com>
2024-02-06 22:43:38 +0200

    Add Label's with game name

--- src/StartFrame.java
+++ src/StartFrame.java
@@ -1,12 +1,12 @@
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
-
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -21,12 +21,11 @@ public class StartFrame extends JFrame
     private static ImageResizer imageResizer;
     private static BufferedImage backgroundImage;
 
-    private StartFrame(String backgroundPath, String playButtonPath) throws IOException
+    private StartFrame(String backgroundPath, String playButtonPath) throws IOException 
     {
         setTitle("Rainy McDonald's The Remake");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setPreferredSize(new Dimension(Settings.FRAME_WIDTH.value, Settings.FRAME_HEIGHT.value));
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setAutoRequestFocus(true);
         setFocusable(true);
         setResizable(false);
@@ -50,26 +49,54 @@ public class StartFrame extends JFrame
                 setBackground(Color.BLACK);
                 setLayout(null);
 
-                add (new JLabel("RAINY MCDONALD'S THE REMAKE") {
+                add(new JLabel() {
                     {
-                        this.setBounds(50,
-                        50, 
-                        100,
-                        30);
+                        setText("===================");
+                        setForeground(new Color(0xFFFF00));
+                        setFont(new Font("Bauhaus 93", Font.BOLD, 50));
+                        setBounds(10, 0, Settings.FRAME_WIDTH.value - 20, 100);
                     }
                 });
 
-                add(new JButton(new ImageIcon(playButtonPath)) {
+                add(new JLabel() {
                     {
-                        this.setBounds(
-                            (Settings.FRAME_WIDTH.value - 200) / 2,
-                            Settings.FRAME_HEIGHT.value / 2 + 100,
-                            200,
-                            100
-                        );  
+                        setText("RAINY MCDONALD'S");
+                        setForeground(new Color(0xFFFF00));
+                        setFont(new Font("Bauhaus 93", Font.BOLD, 60));
+                        setBounds(100, 40, Settings.FRAME_WIDTH.value - 20, 100);
+                    }
+                });
 
-                        this.addActionListener(new ActionListener() {  
-                            public void actionPerformed(ActionEvent actionEvent)
+                add(new JLabel() {
+                    {
+                        setText("THE REMAKE");
+                        setForeground(new Color(0xFFFF00));
+                        setFont(new Font("Bauhaus 93", Font.BOLD, 40));
+                        setBounds(100, 80, Settings.FRAME_WIDTH.value - 20, 100);
+                    }
+                });
+
+                add(new JLabel() {
+                    {
+                        setText("===================");
+                        setForeground(new Color(0xFFFF00));
+                        setFont(new Font("Bauhaus 93", Font.BOLD, 50));
+                        setBounds(10, 120, Settings.FRAME_WIDTH.value - 20, 100);
+                    }
+                });
+
+                add(new JButton(new ImageIcon(playButtonPath))
+                {
+                    {
+                        setBounds(
+                                (Settings.FRAME_WIDTH.value - 200) / 2,
+                                Settings.FRAME_HEIGHT.value / 2 + 100,
+                                200,
+                                100
+                        );
+
+                        addActionListener(new ActionListener() {
+                            public void actionPerformed(ActionEvent actionEvent) 
                             {
                                 started = true;
                             }
@@ -79,7 +106,8 @@ public class StartFrame extends JFrame
             }
 
             @Override
-            protected void paintComponent(Graphics graphics) {
+            protected void paintComponent(Graphics graphics) 
+            {
                 super.paintComponent(graphics);
                 graphics.drawImage(backgroundImage, 0, 0, this);
             }
@@ -90,19 +118,20 @@ public class StartFrame extends JFrame
         started = false;
     }
 
-    public static synchronized StartFrame getInstance(String backgroundPath, String playButtonPath)
+    public static synchronized StartFrame getInstance(String backgroundPath, String playButtonPath) 
     {
-        if (null == instance)
+        if (null == instance) 
         {
-            try
+            try 
             {
                 instance = new StartFrame(backgroundPath, playButtonPath);
-            } 
+            }
             catch (IOException ioe) 
             {
                 ioe.printStackTrace();
             }
         }
+
         return instance;
     }
 
